78f6079faf5f04256b54237da762da0a9ed5269c
Suyel Haque <slsuyel@gmail.com>
2025-10-02 18:12:26 +0600

    fix: update the map section

--- src/app/(withCommonLyout)/page.tsx
+++ src/app/(withCommonLyout)/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <div className="w-full ">
       <HeroSection />
+      <MapSection />
       <Hotels />
       <TouristPlaces />
-      <MapSection />
     </div>
   );
 }
